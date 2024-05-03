cb7a5d0b5f232d63094bfffe6f46bb8cd1e55606
Shivam Singh <shivam003a@gmail.com>
2024-05-03 15:09:32 +0530

    26

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ with cols2:
 st.title("Brain Tumor Segmentation")
 st.sidebar.image("./logo.png", width=120)
 st.sidebar.write("### Early Brain Tumor Detection System using Modified U-Net")
-st.sidebar.write("\nGuided By:  \nAshwini Kumar Upadhyaya  \nAsst. Professor, Rec Kannauj");
-st.sidebar.write("### Project By:  \nShivam Singh(54)  \nDeependu Mishra(28)  \nSudhir Tiwari(59)  \nAshish Yadav(32)  \nNitesh Kumar(39)")
+st.sidebar.write("\n**Guided By:**  \nAshwini Kumar Upadhyaya  \nAsst. Professor, Rec Kannauj");
+st.sidebar.write("\n\n\n**Project By:**  \nShivam Singh(54)  \nDeependu Mishra(28)  \nSudhir Tiwari(59)  \nAshish Yadav(32)  \nNitesh Kumar(39)")
 
 def segment_image(image_np, model_name):
     print("Hello")
